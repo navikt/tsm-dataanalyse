598005d4a7528a1941b755bd63a5c10f08a61f9c
leneomdal <lene.omdal@hotmail.com>
2026-01-07 12:10:58 +0100

    add test flyt from staging to marts

---
+++ dbt/models/intermediate/intermediate_sykmeldinger.sql
@@ -0,0 +1,5 @@
+SELECT 
+    sm.id,
+    DATE(LEFT(sm.mottattDato, 10)) AS mottattDato, 
+    sm.sm_type,
+FROM {{ref('staging_sykmeldinger')}} AS sm
---
+++ dbt/models/marts/marts_antall_per_type.sql
@@ -0,0 +1,7 @@
+SELECT 
+    COUNT(DISTINCT sm.id) AS antall_sm, 
+    sm.sm_type,
+    EXTRACT(YEAR from sm.mottattDato) AS aar
+FROM {{ref('intermediate_sykmeldinger')}} AS sm
+GROUP BY sm_type, aar
+ORDER BY aar
